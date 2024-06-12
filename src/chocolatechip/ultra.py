182b41b0779afc36ced7c506aa04a54d0548fba8
Trey Krause <krausetrey@gmail.com>
2024-06-12 19:20:22 -0400

    rudimentary working analysis

--- src/chocolatechip/ultra.py
+++ src/chocolatechip/ultra.py
@@ -1,26 +1,30 @@
 from ultralytics import YOLOv10
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 def main():
-    data = '/home/trey/exp/yolov10/ultralytics/cfg/models/v10/yolov10s.yaml'
+    data = 'obj.yaml'
     epochs = 100
     imSz = 640
-
+    
     # run python command to train
     model = YOLOv10('yolov10s.pt')
     model.train(data=data, epochs=epochs, imgsz=imSz)
 
-    # read output, save results
-    results = pd.read_csv('results.csv')
-
-    print(results)
+    results = pd.read_csv('~/yolov10/runs/detect/train14/results.csv') # needs to be changed depending on user and run count
+    results.columns = results.columns.str.strip()    
 
     # plot results of loss / epochs
-    plt.figure(10,6)
+    plt.figure(figsize=(10, 6))
     plt.plot(results['epoch'], results['train/cls_om'])     # currently analyzes loss for clasification of the model
-    plt.xlabel('Epochs')
+    plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.title('Epochs vs Loss')
     plt.grid(True)
+    
+    plt.savefig('/home/trey/please/yolov10.png')    # needs to be changed depending on desired save location
     plt.show()
+    
+
+if __name__ == "__main__":
+    main()
