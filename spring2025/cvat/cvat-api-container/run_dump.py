52e2a6eb11182b3a899f48334064692620afa9dd
sargamt <sargamthakur665@gmail.com>
2025-02-07 10:20:13 -0500

    updated dockerfile, makefile, and added python script to list all tasks.

---
+++ spring2025/cvat/cvat-api-container/run_dump.py
@@ -0,0 +1,22 @@
+import subprocess
+
+subprocess.run([
+    "cvat-cli",
+    "--server-host", "maltserver.cise.ufl.edu",
+    "--auth", "REMOVED",
+    "--server-port", "8080",
+    "ls"
+])
+
+# ls works, but 'dump'ing the task is not working for some reason
+'''subprocess.run([
+    "cvat-cli",
+    "--server-host", "maltserver.cise.ufl.edu",
+    "--auth", "REMOVED",
+    "--server-port", "8080",
+    "dump",
+    "--format", "YOLO 1.1",
+    "--with-images", "True",
+    "67",
+    "/app/tasks/67.zip"
+])'''
