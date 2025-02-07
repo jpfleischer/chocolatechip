884d26fc503e40f6e098a3f606732c464fe41790
Julian Dominguez <julian.dominguez@ufl.edu>
2025-02-07 13:45:44 -0500

    fixed darknet container and makefile to work with volume mounts

--- spring2025/darknet/script.sh
+++ spring2025/darknet/script.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
+if [ ! -d "darknet" ]; then
+    git clone https://github.com/hank-ai/darknet.git
+fi
+
 # Set working directory
 cd /workspace/darknet
 
 # Check if the "build" directory exists
-if [ -d "build" ]; then
-    echo "Darknet is already built. Exiting."
-    exit 0
-fi
+if [ ! -d "build" ]; then
+    # Create and enter the build directory
+    mkdir build
+    cd build
 
-# Create and enter the build directory
-mkdir build
-cd build
-
-# Build Darknet
-cmake ..
-make -j$(nproc) package
+    # Build Darknet
+    cmake ..
+    make -j$(nproc) package
+fi 
 
 # Install the package
 dpkg -i /workspace/darknet/build/darknet-*.deb
