32911f184201f8cf390c1cff1d2300d0da2c42e7
jpfleischer <j.fleischer@ufl.edu>
2025-02-18 13:01:51 -0500

    add beginnings of car train

---
+++ spring2025/collab.py
@@ -0,0 +1,22 @@
+import subprocess
+
+# r = subprocess.call('cd cvat/cvat-api-container && make', shell=True,  text=True)
+
+# there should be logic
+# to see if r (the object that is returned by subprocess)
+# has "unzipped"
+#or, better, a sentinel
+# "ALL TASKS COMPLETE!!!"
+# if "ALL TASKS COMPLETE!!!" in r:
+#    proceed.....
+# else:
+#    epic fail
+
+
+print('#'* 90)
+print('now for darknet')
+print('#'* 90)
+
+
+r2 = subprocess.call('cd darknet && make collab', shell=True,  text=True)
+
