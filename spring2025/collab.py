924cb2c5c3885c9f0002de7f627384bda28de4b9
Julian Dominguez <julian.dominguez@ufl.edu>
2025-02-20 15:25:27 -0500

    change collab.py message

--- spring2025/collab.py
+++ spring2025/collab.py
@@ -2,19 +2,10 @@ import subprocess
 
 r = subprocess.call('cd cvat && make', shell=True,  text=True)
 
-# there should be logic
-# to see if r (the object that is returned by subprocess)
-# has "unzipped"
-#or, better, a sentinel
-# "ALL TASKS COMPLETE!!!"
-# if "ALL TASKS COMPLETE!!!" in r:
-#    proceed.....
-# else:
-#    epic fail
-
+# TODO: add error detection for cvat setup
 
 print('#'* 90)
-print('now for darknet')
+print('Darknet Training')
 print('#'* 90)
 
 
