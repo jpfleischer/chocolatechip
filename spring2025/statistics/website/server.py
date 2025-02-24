dfdec5a96730d05bef55417cb354d27e2a855dee
Artur Kumik <artkumik@gmail.com>
2025-02-24 11:30:49 -0500

    flask and reorganize

---
+++ spring2025/statistics/website/server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+#must run using python3 server.py
+
+app = Flask(__name__)
+
+@app.route('/') #localhost
+def home():
+    return render_template('home.html')
+
+@app.route('/cameras') #localhost/cameras
+def cameras():
+    cameras = [{'number':'1','intersection':'Alachua'},
+               {'number':'2','intersection':'Broward'}]
+    return render_template('cameras.html', author = "Artur", cam_active = True, cameras = cameras)
+
+@app.route('/cameras/<camera_id>')
+def cameranum(camera_id):
+    return render_template('cameras_num.html', camera_id = str(camera_id))
+
+#if __name__ == "__main__":
+    app.run()
+
+#to run with gunicorn gunicorn -w 4 -b 0.0.0.0:8000 server:app
+#pkill gunicorn to kill processes related to gunicorn (might be dangerou)
