163f2bd9fee95930748a1427c194179c19e5db45
jpfleischer <70083705+stapmoshun@users.noreply.github.com>
2025-03-31 15:05:19 -0700

    add username machine pairing to plot

---
+++ spring2025/darknet/LegoGearsFiles/plot/username_machine_pairings.py
@@ -0,0 +1,11 @@
+def get_machine(username):
+    machine_map = {
+        "akumik": "HiPerGator (UF)",
+        "thakur.s": "HiPerGator (UF)",
+        "julian.dominguez": "HiPerGator (UF)",
+        "pnh2pj": "Afton (UVA)",
+        "bdu4xa": "Afton (UVA)",
+        "ukx5fv": "Afton (UVA)",
+        "jpf": "MALTLab"
+    }
+    return machine_map.get(username, "Personal Machine")
