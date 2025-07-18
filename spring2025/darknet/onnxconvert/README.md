52d2974ea688c8828eca5c5f9c954aa31970a484
jpfleischer <70083705+stapmoshun@users.noreply.github.com>
2025-07-17 20:35:49 -0700

    simplify the onnx conversion

---
+++ spring2025/darknet/onnxconvert/README.md
@@ -0,0 +1,9 @@
+all that is needed to be changed is the convert.sh.
+
+change the first two variables, MODEL_NAME and MODEL_DIR.
+
+it is expected that the .weights are immediately available in the
+specified MODEL_DIR and not nested in any subdirectories.
+
+there should also only be one .cfg file.
+
--- spring2025/darknet/onnxconvert/convert.sh
+++ spring2025/darknet/onnxconvert/convert.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 
-MODEL_NAME="annotations_final"
-MODEL_DIR="/data/traffic_training_cvat_unzipped/annotations"
+#MODEL_NAME="combined_best"
+#MODEL_DIR="/data/ke_images_newrepo/darknet/data/combined"
+MODEL_NAME="yash_images_best"
+MODEL_DIR="/data/yash_images/"
 
 mkdir -p output
 
 echo "Copying model files from ${MODEL_DIR}..."
 cp ${MODEL_DIR}/${MODEL_NAME}.weights .
-cp ${MODEL_DIR}/annotations.cfg ${MODEL_NAME}.cfg
+cp ${MODEL_DIR}/*.cfg ${MODEL_NAME}.cfg
 
 echo "Converting DarkNet model to ONNX..."
 python3 yolo2onnx.py -c ${MODEL_NAME}.cfg -w ${MODEL_NAME}.weights -o output
