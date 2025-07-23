9c2714700e2f1e44a3a16fedfe9b01f36523c15d
J.P. <stapmoshun@gmail.com>
2025-07-23 01:17:47 -0400

    add total hours and weekdays counter

---
+++ src/chocolatechip/intersections.py
@@ -0,0 +1,9 @@
+
+intersection_lookup = {
+    3287: "Stirling Road and N 68th Avenue",
+    3248: "Stirling Road and N 66th Avenue",
+    3032: "Stirling Road and SR-7",
+    3265: "Stirling Road and University Drive",
+    3334: "Stirling Road and SW 61st Avenue",
+    3252: "Stirling Road and Davie Road Extension"
+}
